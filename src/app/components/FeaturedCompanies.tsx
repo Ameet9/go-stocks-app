c0005df40aa4bbfb41973d60175553e63d60fd2c
Ameet9 <ameetnayak919@gmail.com>
2023-07-03 11:24:48 +0530

    fix updated

--- src/app/components/FeaturedCompanies.tsx
+++ src/app/components/FeaturedCompanies.tsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import dummyLogos from "../rawData/DummyLogos";
 import Image from 'next/image';
 
 export default function FeaturedCompanies() {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
     return (
         <>
             <div className='bg-gray-300'>
-                {window.innerWidth >= 768 &&
+                {isClient && window.innerWidth >= 768 &&
                     <div className='p-2'>
                         <h1 className='font-bold text-red-500'>FEATURED COMPANIES</h1>
                     </div>
                 }
                 <div className='grid grid-cols-5 md:grid-cols-10 gap-2 sm:gap-3 bg-cyan-100 p-3'>
-                    {dummyLogos.slice(0, window.innerWidth >= 768 ? 10 : 5).map((s) => (
+                    {dummyLogos.slice(0, isClient && window.innerWidth >= 768 ? 10 : 5).map((s) => (
                         <div key={s.id} className=''>
                             <Image alt='' src={s.url} className='w-20 h-20 sm:w-16 sm:h-16'></Image>
                         </div>
